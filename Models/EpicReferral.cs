20b674a905d25fd2681bfda43b7b85620f8e7edd
Ian Harriott <warriorbydawn@hotmail.com>
2020-12-29 18:41:18 +0000

    Epic functionality, AuthConverter

---
+++ Models/EpicReferral.cs
@@ -0,0 +1,10 @@
+public class EpicReferral
+{
+    public string CreateDate { get; set; }
+    public string EpicReferralID { get; set; }
+    public string MemberFName { get; set; }
+    public string MemberLName { get; set; }
+    public string ReqProvSpec { get; set; }
+    public string ReqProvSpecDesc { get; set; }
+    public string MemberMRN { get; set; }
+}
